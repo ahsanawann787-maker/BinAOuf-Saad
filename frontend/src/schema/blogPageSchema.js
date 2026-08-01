0ad38e59443f4b17f798905f2271325a19693c89
ahsanawann787-maker <ahsanawann787@gmail.com>
2026-08-01 09:42:10 -0700

    Add Blog page schema

---
+++ frontend/src/schema/blogPageSchema.js
@@ -0,0 +1,9 @@
+const blogPageSchema = {
+  "@context": "https://schema.org",
+  "@type": "Blog",
+  "name": "Bin Aouf Blog",
+  "description": "Learn about Himalayan Pink Salt, export, manufacturing, health benefits, and industry insights from Bin Aouf.",
+  "url": "https://www.binaouf.com/blog"
+};
+
+export default blogPageSchema;
